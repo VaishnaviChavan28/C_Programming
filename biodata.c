411b32b7454c7bf221e377090b214f2a7644f243
VaishnaviChavan28 <chavanvaishnavi331@gmail.com>
2026-06-27 23:49:04 +0530

    Bio data printing using C

---
+++ biodata.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    printf("\n\t\t---------BIODATA---------\n\t\t");
+    printf("\n\t\tName \t :\t Vaishnavi Chavan");
+    printf("\n\t\tAge \t :\t 20");
+    printf("\n\t\tAddress  :\t Wadaki");
+    printf("\n\t\tCity \t :\t Pune");
+    printf("\n\t\tPhoneno. :\t 1234567890");
+    printf("\n\t\tHobby \t :\t Teaching");
+    printf("\n\t\tEmail \t :\t abc@gmail.com");
+
+    printf("\n\n\t\t---------Thank You---------\n\t\t");   
+   
+}
